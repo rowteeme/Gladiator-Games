6e30f42576c6d0865ec5a54f68f7eb3517994667
Rotimi Oyewole <Rotimi.Oyewole@gmail.com>
2013-12-10 16:43:42 -0500

    Added Redirect code for Submit page as Default

--- amazonnb.js
+++ amazonnb.js
@@ -1,5 +1,12 @@
 // Redirect users from Submit page to About page
 
+jQuery(function($){
+	//.SDescription only exists on default About Tab - Do Not Edit
+	if ($('.SDescription').length > 0){
+	var enter = $('#cnav_submit').attr('href');
+	window.open( enter , '_self');
+	}
+});
 
 
 // Inserting and Moving Around Divs
