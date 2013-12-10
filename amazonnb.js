7a4ccc65ee06932f62ec0a445ae8d1bd5e6cc8dd
Rotimi Oyewole <Rotimi.Oyewole@gmail.com>
2013-12-10 16:20:04 -0500

    Created a JS File
    #sosexy

---
+++ amazonnb.js
@@ -0,0 +1,14 @@
+// Redirect users from Submit page to About page
+
+
+
+// Inserting and Moving Around Divs
+
+jQuery(function($) {
+	$('#elimination').insertAfter('.CHeader');
+	$('#cnav_submit').prependTo('.CNav');
+});
+
+//iFrame Gallery to bottom of experience
+
+
