f47520282a1cda239840d5db770b58323ab56492
Rotimi Oyewole <Rotimi.Oyewole@gmail.com>
2013-12-10 22:11:46 -0500

    Added and styled Upload Methods section
    Max, I’ll slit your nuts if you fuck this up. Oh wait! We have Github
    now. Nevermind :)
    
    Things I told them I’d have by tomorrow:
    - Google Fonts they can use as alternative to fonts they have (Proxima
    Nova for body)
    - Mockup of what the modal experience will look like. Already hid all
    the fields, just need to move all the form-related divs and inputs to a
    modal (append to a new div) and create the background experience like
    Roblum mentioned.

--- amazonnb.js
+++ amazonnb.js
@@ -14,9 +14,11 @@ jQuery(function($){
 jQuery(function($) {
 	// Insert the Participate Instructions below .SBox
 	$('<div id="participate"></div>').insertBefore('.SRules');
+	// Create a div called Submit Section and append to .SBox
+	$('<div id="submit-section"><h1>Tag your entry with #NBGladiator or upload from the options below</h1><div id="upload-methods"><img id="computer-upload" class="social-cta" src="https://opop.cachefly.net/amazonnb/upload.png" /><img id="facebook-upload" class="social-cta" src="https://opop.cachefly.net/amazonnb/facebook.png" /><img id="instagram-cta" class="social-cta" src="https://opop.cachefly.net/amazonnb/instagram.png" /></div></div>').insertAfter('#instructions');
 });
 
-//Pre-Populate Fields that aren't being used with dummy text
+// Pre-Populate Fields that aren't being used with dummy text
 
 jQuery(function($){
 	$('#caption').attr('value', 'Caption');
@@ -25,7 +27,7 @@ jQuery(function($){
 
 
 
-//On DOM Ready function to get rid of all the crap in the footer
+// On DOM Ready function to get rid of all the crap in the footer
 jQuery(function($){
 
 	$(document).ready(function(){
@@ -36,7 +38,3 @@ jQuery(function($){
 	});
 
 });
-
-
-
-
