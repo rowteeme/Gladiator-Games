2c3302077edf350d069624cab7b9aaa17552d95e
MaxOfferpop <mbaldwin@offerpop.com>
2013-12-13 11:46:22 -0500

    Adding Bracket Folder to build
    I started a folder for the bracket. It an HTML, CSS and JS file for the
    bracket.

---
+++ bracket/functions.js
@@ -0,0 +1,16 @@
+        jQuery(document).ready(function($){
+            
+            var columnOne = $("#column-1").height();
+            var columnTwo = $("#column-2").height();
+            var columnThree = $("#column-3").height();
+            var columnFour = $("#column-4").height();
+            
+            var medium  = (columnOne - columnTwo)/2;
+            var small = (columnOne - columnThree)/2;
+            var center = (columnOne - columnFour)/2;
+            
+            $(".medium").css("margin-top", medium);
+            $(".short").css("margin-top", small);
+            $(".center").css("margin-top", center);
+            
+        });
