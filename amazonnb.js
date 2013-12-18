7ff6e5c33fb993a7b1146456b14b51e2be1c62e4
Rob <lum.robert@gmail.com>
2013-12-18 15:54:48 -0500

    Cleaned up jQuery functions + added popbox 2 for IG & TW
    deleted some unnecessary function wraps and added pop box2 for
    instagram and twitter modal.

--- amazonnb.js
+++ amazonnb.js
@@ -1,69 +1,59 @@
-// Redirect users from Submit page to About page
-
 jQuery(function($){
+	// Redirect users from Submit page to About page
 	if ($('.SDescription').length > 0){
-	var enter = $('#cnav_submit').attr('href');
-	window.open( enter , '_self');
+		var enter = $('#cnav_submit').attr('href');
+		window.open( enter , '_self');
 	}
-});
 
+	// On DOM Ready function to get rid of all the crap in the footer
+	$("div.CFooterDivider").next().hide();
+	$("div.CFooter").prev().remove();
 
+		// Pre-Populate Fields that aren't being used with dummy text
+		$('#caption').attr('value', 'LET THE GAMES BEGIN! The NB Gladiator Games are underway – unleash your inner gladiator for a chance to win a 10-day VIP experience to Rome for two, plus entries to run the legendary Rome Marathon, courtesy of @newbalance. Enter now.');
+		$('#city').attr('value', 'City');
+		// Inserting and Moving Around Divs
+		// Insert the Participate Instructions below .SBox
+		$('<div id="participate"></div>').insertAfter('.SBox');
+		// Create a div called Submit Section and insert after Instructions
+		// $('<div id="submit-section"><h1>Tag your entry with #NBGladiator or upload from the options below</h1><div id="upload-methods"><img id="computer-upload" class="social-cta" src="https://opop.cachefly.net/amazonnb/upload.png" /><img id="facebook-upload" class="social-cta" src="https://opop.cachefly.net/amazonnb/facebook.png" /><img id="instagram-cta" class="social-cta" src="https://opop.cachefly.net/amazonnb/instagram.png" /></div></div>').insertAfter('#instructions');
+		
+		//Insert the Gallery beneath the Submit page
+		$('<iframe src="https://offerpop.com/Contest.psp?c=512215&amp;u=54823&amp;a=281603285304794&amp;p=643783498974952&amp;rest=0&amp;v=View" id="bottomgallery" width="100%" height="600"/>').insertAfter('#participate'); 
+		
+		//Insert Submit button
+		$('#form_submit_button').html('<img src="https://opop.cachefly.net/amazonnb/submit.png" />');
 
-// Inserting and Moving Around Divs
+			//Hide the Tab image on the Gallery page and style changes
+			if ($('#gallery-controls').length > 0) {
+					$('.CHeader').hide();
+				} else if ($('#sharebar').length > 0) {
+					$('.CHeader').hide();
+					$('.CSub').css('color', 'white');
+					$('.TInstructions').css('color', 'white');
+					$('.bitlyLink').css('color', 'white');
+					$('#share-option-facebook a').css('color', 'white');
+					$('#share-option-twitter a').css('color', 'white');
+					$('#share-option-email a').css('color', 'white');
+			}
 
-jQuery(function($) {
-	// Insert the Participate Instructions below .SBox
-	$('<div id="participate"></div>').insertAfter('.SBox');
-	// Create a div called Submit Section and insert after Instructions
-	// $('<div id="submit-section"><h1>Tag your entry with #NBGladiator or upload from the options below</h1><div id="upload-methods"><img id="computer-upload" class="social-cta" src="https://opop.cachefly.net/amazonnb/upload.png" /><img id="facebook-upload" class="social-cta" src="https://opop.cachefly.net/amazonnb/facebook.png" /><img id="instagram-cta" class="social-cta" src="https://opop.cachefly.net/amazonnb/instagram.png" /></div></div>').insertAfter('#instructions');
-	
-	//Insert the Gallery beneath the Submit page
-	$('<iframe src="https://offerpop.com/Contest.psp?c=512215&amp;u=54823&amp;a=281603285304794&amp;p=643783498974952&amp;rest=0&amp;v=View" id="bottomgallery" width="100%" height="600"/>').insertAfter('#participate'); 
-	
-	//Insert Submit button
-	$('#form_submit_button').html('<img src="https://opop.cachefly.net/amazonnb/submit.png" />');
-
-	//Hide the Tab image on the Gallery page and style changes
-	if ($('#gallery-controls').length > 0) {
-		$('.CHeader').hide();
-	} else if ($('#sharebar').length > 0) {
-		$('.CHeader').hide();
-		$('.CSub').css('color', 'white');
-		$('.TInstructions').css('color', 'white');
-		$('.bitlyLink').css('color', 'white');
-		$('#share-option-facebook a').css('color', 'white');
-		$('#share-option-twitter a').css('color', 'white');
-		$('#share-option-email a').css('color', 'white');
-	}
-});
-
-// Elimination Round
-jQuery(function($){
+	// Elimination Round
 	$('.playerThumb a').mouseenter(function(){
 			$(this).next().show();
 	});
 
-		$('.playerThumb a').mouseleave(function(){
+	$('.playerThumb a').mouseleave(function(){
 			$(this).next().hide();
 	});
 
+	//Create Back to Gallery Button
+	var galleryPage = $('#cnav_view').attr('href');
+	$('<div id="goBack"><a href="' + galleryPage + '" target="_self"><p>Back to Gallery</p></a></div>').insertAfter('#sharebar');
+
 });
-
-
-// Pre-Populate Fields that aren't being used with dummy text
-
-jQuery(function($){
-	$('#caption').attr('value', 'LET THE GAMES BEGIN! The NB Gladiator Games are underway – unleash your inner gladiator for a chance to win a 10-day VIP experience to Rome for two, plus entries to run the legendary Rome Marathon, courtesy of @newbalance. Enter now.');
-	$('#city').attr('value', 'City');
-});
-
-
-	
 
 // Placeholder Text for Form Fields
-
 jQuery(function($){
-
 	// Reorder inputs and Placeholders for Submit Form
 	$('#sfield_name').insertAfter('#sfield_lastname');
 	$("#email").attr('placeholder', 'Email address');
@@ -95,22 +85,6 @@ $('[placeholder]').focus(function() {
     input.val(input.attr('placeholder'));
   }
 }).blur();
-});
-
-
-
-
-// On DOM Ready function to get rid of all the crap in the footer
-jQuery(function($){
-
-	$(document).ready(function(){
-	
-		$("div.CFooterDivider").next().hide();
-		$("div.CFooter").prev().remove();
-		
-
-	});	
-
 });
 
 
@@ -163,14 +137,14 @@ jQuery(function($) {
 	});
 
 	$('#fadebg , .closebutton').click(function() {  
-		$('#popbox1, #fadebg').fadeOut()
+		$('#popbox1, #fadebg, #popbox2').fadeOut()
 		return false;
 	});
 });
 
 jQuery(function($){
+//Resize Modal based on user image size
 if ($('#frmSignUp').length > 0 ) {
-
         //Take height of cropbox & resize modal
         var resize = function(){
                 var nheight = parseInt(document.getElementById('cropbox').style.height);
